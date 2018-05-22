95e858075515e1f7e2cd4c646a62b8ca89463920
Benoit Rion <benoitrion@gmail.com>
2018-05-23 00:10:42 +0200

    Setup mongodb connection

--- server.js
+++ server.js
@@ -1,7 +1,8 @@
 var express = require('express'),
     stylus = require('stylus'),
     logger = require('morgan'),
-    bodyParser = require('body-parser');
+    bodyParser = require('body-parser'),
+    mongoose = require('mongoose');
 
 var env = process.env.NODE_ENV = process.env.NODE_ENV || 'development';
 
@@ -25,13 +26,32 @@ app.use(stylus.middleware(
 
 app.use(express.static(__dirname + '/public'));
 
+//mongoose.connect('mongodb://localhost/multivision');
+//mongoose.connect('mongodb://root:password@127.0.0.1:27017/multivision');
+mongoose.connect('mongodb://127.0.0.1:27017/multivision');
+
+var db = mongoose.connection;
+db.on('error', console.error.bind(console, 'connection error...'));
+db.once('open', function callback() {
+  console.log('multivision db opened');
+});
+var messageSchema = mongoose.Schema({message: String});
+var Message = mongoose.model('Message', messageSchema);
+var mongoMessage;
+Message.findOne().exec(function(err, messageDoc) {
+  mongoMessage = messageDoc.message;
+});
+
+
 app.get('/partials/:partialPath', function(req, res) {
   console.log('in controller partials/' + req.params.partialPath)
   res.render('partials/' + req.params.partialPath);
 })
 
 app.get('*', function(req, res) {
-    res.render('index');
+    res.render('index', {
+      mongoMessage: mongoMessage
+    });
 });
 
 var port = 3030;
