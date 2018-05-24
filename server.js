fd293a429039a2ac88cb7be4d4af769fb9c74ecf
Benoit Rion <benoitrion@gmail.com>
2018-05-24 21:54:39 +0200

    Designing UI

--- server.js
+++ server.js
@@ -39,13 +39,6 @@ db.on('error', console.error.bind(console, 'connection error...'));
 db.once('open', function callback() {
   console.log('multivision db opened');
 });
-var messageSchema = mongoose.Schema({message: String});
-var Message = mongoose.model('Message', messageSchema);
-var mongoMessage;
-Message.findOne().exec(function(err, messageDoc) {
-  mongoMessage = messageDoc.message;
-});
-
 
 app.get('/partials/:partialPath', function(req, res) {
   console.log('in controller partials/' + req.params.partialPath)
@@ -53,9 +46,7 @@ app.get('/partials/:partialPath', function(req, res) {
 })
 
 app.get('*', function(req, res) {
-    res.render('index', {
-      mongoMessage: mongoMessage
-    });
+    res.render('index');
 });
 
 var port = process.env.PORT || 3030;
